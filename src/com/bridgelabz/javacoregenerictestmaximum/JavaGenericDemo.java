17c892ee1260d01f786999c3079a2cba01e5711e
AshwiniShirsath <shirsathashwini9495@gmail.com>
2022-04-21 00:43:13 +0530

    [ADDED] : Checking Maximum from Three String Object

--- src/com/bridgelabz/javacoregenerictestmaximum/JavaGenericDemo.java
+++ src/com/bridgelabz/javacoregenerictestmaximum/JavaGenericDemo.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.javacoregenerictestmaximum;
 /*
  * @author : Ashwini
- * Given 3 Floats find the maximum
+ * Given 3 Strings find the maximum
  * - Ensure to test code with the Test Case and repeat with the similar 3 Test cases in UC 1
+ * - E.g Apple Peach Banana
  */
 public class JavaGenericDemo {
 	//compareTo method for Checking Maximum from 3 Integer Object
